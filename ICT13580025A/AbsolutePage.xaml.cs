16a43e11b623e7a586b9d0ffc3a919a6ae6c1f2e
ncnmon <33740511+ncnmon@users.noreply.github.com>
2017-12-21 09:38:30 +0700

    Init

---
+++ ICT13580025A/AbsolutePage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ICT13580025A
+{
+    public partial class AbsolutePage : ContentPage
+    {
+        public AbsolutePage()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
